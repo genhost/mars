55e981821aea7261947dedfc390ec64f4e02e8a4
Mark Meliksetyan <markmelix@gmail.com>
2023-03-16 17:34:51 +0300

    full app with index page showing blog posts

--- mars/main.py
+++ mars/main.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from data import db_session
+from data.news import News
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "yandexlyceum_secret_key"
 
 
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
 def main():
+    db_session.global_init("db/blogs.db")
     app.run()
 
 
